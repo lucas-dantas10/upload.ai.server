7b585b1a8b1711677e17735249173e714bf12b7e
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2023-09-15 12:31:46 -0300

    Mudança no nome da requisicao recebida

--- src/routes/generate-ai-completion.ts
+++ src/routes/generate-ai-completion.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
 import { z } from 'zod';
+import { streamToResponse, OpenAIStream } from 'ai';
 import { createReadStream } from 'node:fs';
 import { openai } from "../lib/openai";
 
@@ -9,11 +10,14 @@ export async function generateAiCompletionRoute(app: FastifyInstance) {
 
         const bodySchema = z.object({
             videoId: z.string().uuid(),
-            template: z.string(),
+            prompt: z.string(),
             temperature: z.number().min(0).max(1).default(0.5)
         });
 
-        const  { videoId, template, temperature } = bodySchema.parse(req.body);
+
+        const  { videoId, prompt, temperature } = bodySchema.parse(req.body);
+
+        console.log(temperature, videoId);
 
         const video = await prisma.video.findFirstOrThrow({
             where: {
@@ -25,16 +29,24 @@ export async function generateAiCompletionRoute(app: FastifyInstance) {
             return rep.status(400).send({ error: "Video transcription was not generate yet." });
         }
 
-        const promptMessage = template.replace('{transcription}', video.transcription);
+        const promptMessage = prompt.replace('{transcription}', video.transcription);
 
         const response = await openai.chat.completions.create({
             model: 'gpt-3.5-turbo-16k',
             temperature,
             messages: [
                 { role: 'user', content: promptMessage }
-            ]
+            ],
+            stream: true
         });
 
-        return response;
+        const stream = OpenAIStream(response);
+
+        streamToResponse(stream, rep.raw, {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Method': 'GET, POST, PUT, DELETE, OPTIONS',
+            }
+        });
     });
 }
